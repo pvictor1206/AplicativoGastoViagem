c793e8048946880e2a5979bd9c3908bfb80bbe00
Paulo Victor Santos Magalhães <paulomagalhaes1206@gmail.com>
2021-11-15 18:09:39 -0300

    commit

--- app/src/main/java/com/example/gostodeviagem/MainActivity.kt
+++ app/src/main/java/com/example/gostodeviagem/MainActivity.kt
@@ -2,10 +2,64 @@ package com.example.gostodeviagem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.NumberFormatException
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonCalculate.setOnClickListener(this)
+
     }
+
+    override fun onClick(view: View) {
+        val id = view.id
+        if(id == R.id.buttonCalculate){
+            calculate()
+        }
+    }
+
+    private fun calculate(){
+        if(validationOK()) {
+            // Valores validos dentro dos calculos
+            try {
+
+                val distance = editDistance.text.toString().toFloat()
+                val price = editPrice.text.toString().toFloat()
+                val autonomy = editAutonomy.text.toString().toFloat()
+
+                val totalValue = (distance * price) / autonomy
+                textTotalValue.text = "RS: ${"%.2f".format(totalValue)}"
+
+            } catch (nfe: NumberFormatException) {
+                Toast.makeText(this,getString(R.string.informe_valores_validos), Toast.LENGTH_LONG).show()
+
+            }
+
+
+            val distance = editDistance.text.toString().toFloat()
+            val price = editPrice.text.toString().toFloat()
+            val autonomy = editAutonomy.text.toString().toFloat()
+
+            val totalValue = (distance * price) / autonomy
+            textTotalValue.text = "RS: ${"%.2f".format(totalValue)}"
+        } else {
+            Toast.makeText(this, getString(R.string.preencha_todos_campos), Toast.LENGTH_LONG).show()
+        }
+
+
+    }
+
+    private fun validationOK(): Boolean{
+        if(editDistance.text.toString() != ""
+            && editPrice.text.toString() != ""
+            && editAutonomy.text.toString() != "")
+            return true
+        return false
+    }
+
 }
